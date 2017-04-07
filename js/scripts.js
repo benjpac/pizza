54ddc417b377bbf891c307417045e26de3a363fa
Ben Metzger <benjpac@gmail.com>
2017-04-07 09:14:39 -0700

    add some of the back end logic for ordering a pizza with cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,44 @@
 // Create a pizza object constructor with properties for toppings and size.
 // Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
 
-function Pizza() {
-  this.
+// If you finish meeting the objectives for this project, consider adding additional features, such as:
+//
+// Style your site with CSS and images.
+// Allow users to order more than one pizza with different toppings.
+// Display the list of pizzas ordered as links that can be clicked for details.
+// Offer a delivery option that then requires address information.
+
+var sizePrices = {
+                   small: 10,
+                   medium: 12,
+                   large: 14,
+                   extra_large: 16
+                 }
+var toppingPrices = {
+                      pepperoni: 1,
+                      sausage: 1,
+                      chicken: 1,
+                      olives: .75,
+                      onions: .75,
+                      bell_peppers: .75
+                    }
+
+function Pizza(size, toppings, delivery) {
+  this.size = size;
+  this.toppings = toppings;
+  this.delivery = delivery;
+  this.cost = 0;
+  this.calcCost = function() {
+    this.cost += sizePrices[this.size];
+    this.toppings.forEach(function(topping) {
+      this.cost += toppingPrices[topping];
+    })
+  }
+}
+
+function Delivery(street, ) {
+  this.street
+  this.address =
 }
 
 $(document).ready(function() {
